f5c6fb75e5f854a2bc908e6bef2fdad8b2d23039
habibliu <740033486@qq.com>
2018-07-19 23:25:08 +0800

    生成环境联调

--- sunjoy-trm-web-master/src/main/java/com/sunjoy/trm/web/base/LoginController.java
+++ sunjoy-trm-web-master/src/main/java/com/sunjoy/trm/web/base/LoginController.java
@@ -1,11 +1,16 @@
 package com.sunjoy.trm.web.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sunjoy.common.auth.service.ISecurityService;
+import com.sunjoy.framework.client.dto.Response;
 
 /**
  *
@@ -14,13 +19,23 @@ import com.sunjoy.common.auth.service.ISecurityService;
  */
 @RestController
 public class LoginController {
+	
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private ISecurityService securityService;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "Organization";
+    public Response loginPage(@RequestParam(name = "username") String username) {
+		logger.info("{} is logining....",username);
+		Response response = new Response();
+		UserDetails user=securityService.loadUserByUsername(username);
+		if(user!=null){
+			response.setData(user);
+		}else{
+			response.setCode("000001");
+		}
+        return response;
     }
 
 }
