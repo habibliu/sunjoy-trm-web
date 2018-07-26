875980e396969bb33c28d9560c600df5fed59527
habibliu <740033486@qq.com>
2018-07-26 20:00:50 +0800

    增加赠送规则表

--- script/biz.sql
+++ script/biz.sql
@@ -61,3 +61,44 @@ COMMENT ON COLUMN "biz"."shift_results"."coach_id" IS '教练ID';
 CREATE UNIQUE INDEX "IDX_PK_SHIFT_RESULTS" ON "biz"."shift_results" USING btree ("id");
 
 ALTER TABLE "biz"."shift_results" ADD PRIMARY KEY ("id");
+
+
+-- ----------------------------
+-- Table structure for present_rule
+-- ----------------------------
+DROP TABLE IF EXISTS "biz"."present_rule";
+CREATE TABLE "biz"."present_rule" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "course_id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "sign_up_periods" int4,
+  "present_sections" int4,
+  "memo" varchar(255) COLLATE "pg_catalog"."default",
+  "status" varchar(16) COLLATE "pg_catalog"."default" NOT NULL,
+  "create_time" timestamp(6),
+  "create_user" varchar(32) COLLATE "pg_catalog"."default",
+  "update_time" timestamp(6),
+  "update_user" varchar(32) COLLATE "pg_catalog"."default"
+)
+;
+COMMENT ON COLUMN "biz"."present_rule"."course_id" IS '课程ID';
+COMMENT ON COLUMN "biz"."present_rule"."sign_up_periods" IS '报名期数';
+COMMENT ON COLUMN "biz"."present_rule"."present_sections" IS '赠送节数';
+COMMENT ON COLUMN "biz"."present_rule"."memo" IS '备注';
+COMMENT ON COLUMN "biz"."present_rule"."status" IS '状态';
+
+-- ----------------------------
+-- Indexes structure for table present_rule
+-- ----------------------------
+CREATE UNIQUE INDEX "IDX_PK_PRESENT_RULE" ON "biz"."present_rule" USING btree (
+  "id" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Primary Key structure for table present_rule
+-- ----------------------------
+ALTER TABLE "biz"."present_rule" ADD CONSTRAINT "present_rule_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Foreign Keys structure for table present_rule
+-- ----------------------------
+ALTER TABLE "biz"."present_rule" ADD CONSTRAINT "FK_TO_COURSE" FOREIGN KEY ("course_id") REFERENCES "mas"."course" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
