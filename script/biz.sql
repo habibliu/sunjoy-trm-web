7482090db6b37b7f763cfcaee8b5d0d1fb5892fa
habibliu <740033486@qq.com>
2018-08-06 01:20:35 +0800

    增加排班及排班结果管理controller类

--- script/biz.sql
+++ script/biz.sql
@@ -36,14 +36,15 @@ CREATE UNIQUE INDEX "IDX_PK_REGISTION" ON "biz"."registion" USING btree ("id");
 
 ALTER TABLE "biz"."registion" ADD PRIMARY KEY ("id");
 
-DROP TABLE IF EXISTS "biz"."shift_results";
-CREATE TABLE "biz"."shift_results" (
+DROP TABLE IF EXISTS "biz"."shift";
+CREATE TABLE "biz"."shift" (
 "id" varchar(32) NOT NULL,
 "train_date" date NOT NULL,
 "train_time" varchar(16) NOT NULL,
 "course_id" varchar(32) NOT NULL,
 "student_id" varchar(32) NOT NULL,
 "coach_id" varchar(32) NOT NULL,
+"venue_id" varchar(32) NOT NULL,
 "status" varchar(16) NOT NULL,
 "create_time" timestamp(6),
 "create_user" varchar(32),
@@ -53,14 +54,15 @@ CREATE TABLE "biz"."shift_results" (
 WITH (OIDS=FALSE)
 
 ;
-COMMENT ON COLUMN "biz"."shift_results"."train_date" IS '训练日期';
-COMMENT ON COLUMN "biz"."shift_results"."course_id" IS '课程ID';
-COMMENT ON COLUMN "biz"."shift_results"."student_id" IS '学员ID';
-COMMENT ON COLUMN "biz"."shift_results"."train_time" IS '训练时间';
-COMMENT ON COLUMN "biz"."shift_results"."coach_id" IS '教练ID';
-CREATE UNIQUE INDEX "IDX_PK_SHIFT_RESULTS" ON "biz"."shift_results" USING btree ("id");
+COMMENT ON COLUMN "biz"."shift"."train_date" IS '训练日期';
+COMMENT ON COLUMN "biz"."shift"."course_id" IS '课程ID';
+COMMENT ON COLUMN "biz"."shift"."train_time" IS '训练时间';
+COMMENT ON COLUMN "biz"."shift"."coach_id" IS '教练ID';
+COMMENT ON COLUMN "biz"."shift"."venue_id" IS '场地ID';
 
-ALTER TABLE "biz"."shift_results" ADD PRIMARY KEY ("id");
+CREATE UNIQUE INDEX "IDX_PK_SHIFT" ON "biz"."shift" USING btree ("id");
+
+ALTER TABLE "biz"."shift" ADD PRIMARY KEY ("id");
 
 
 -- ----------------------------
--- sunjoy-trm-web-master/src/main/java/com/sunjoy/trm/web/bizcore/ShiftController.java
+++ sunjoy-trm-web-master/src/main/java/com/sunjoy/trm/web/bizcore/ShiftController.java
@@ -1,9 +1,50 @@
 package com.sunjoy.trm.web.bizcore;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sunjoy.framework.client.dto.Response;
+import com.sunjoy.framework.dao.paging.Page;
+import com.sunjoy.framework.dao.paging.PageInfo;
+import com.sunjoy.framework.service.controller.WebController;
+import com.sunjoy.framework.utils.BeanUtils;
+import com.sunjoy.trm.bizcore.dao.criteria.RuleCriteria;
+import com.sunjoy.trm.bizcore.dao.dto.RuleDto;
+import com.sunjoy.trm.bizcore.dao.dto.ShiftDto;
+import com.sunjoy.trm.bizcore.dao.entity.Rule;
+import com.sunjoy.trm.bizcore.service.IScheduelService;
+import com.sunjoy.trm.bizcore.service.IShiftService;
+
 /**
  *
  * @author liuganchao<740033486@qq.com>
  * @date 2018年6月27日
  */
-public class ShiftController {
-
+@RestController
+@RequestMapping(value = "/Shift")
+public class ShiftController extends WebController{
+	@Autowired
+	private IShiftService shiftService;
+	/**
+	 * 分页查询
+	 * 
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
+	public Response listRuleByPage(@RequestParam(name = "params") String params,@RequestParam(name = "page") String pageInfo) {
+		Response response = new Response();
+		ShiftDto criteria = JSONObject.parseObject(params, ShiftDto.class);
+		PageInfo page=JSONObject.parseObject(pageInfo, PageInfo.class);
+		Page<ShiftDto> returnPage = shiftService.query(criteria,page);
+		response.setData(returnPage);
+		return response;
+	}
 }
